9758191a8db0b7fba55e90892dc958343f459107
Rug <rugbugredfern@gmail.com>
2021-04-05 09:57:41 -0700

    new update with player count

--- place/table.js
+++ place/table.js
@@ -14,16 +14,22 @@ const interactInterval = 0.5;
 
 var lastInteractTime = 0;
 
-downloadGrid();
+var ignoreCell = { c: -1, r: -1, color: null }
+var hoverCell = { c: -1, r: -1, cell: null };
+var pixels = [];
 
-window.setInterval(() => { downloadGrid(); }, 250);
+getGrid();
+getPlayerCount();
+
+window.setInterval(() => { getGrid(); }, 500);
 window.setInterval(() => { renderProgress(); }, 10);
+window.setInterval(() => { getPlayerCount(); }, 25000);
 
-function downloadGrid() {
+function getGrid() {
 	var request = new XMLHttpRequest();
 	request.open('GET', url + 'api/pixels/');
 	request.addEventListener('load', function() {
-		var pixels = JSON.parse(this.responseText);
+		pixels = JSON.parse(this.responseText);
 		makeGrid(pixels);
 	});
 	request.send();
@@ -35,13 +41,22 @@ function makeGrid(pixels) {
 	for (let r = 0; r < height; r++){
 		const row = table.insertRow(r);
         for (let c = 0; c < width; c++){
-            const cell = row.insertCell(c);
-			cell.setAttribute('style', `background-color: ${pixels[c + r * width]}`);
-            cell.addEventListener("click", function() {
+			const cell = row.insertCell(c);
+			if(ignoreCell.c === c && ignoreCell.r === r) {
+				cell.setAttribute('style', `background-color: ${ignoreCell.color}`);
+				ignoreCell = { c: -1, r: -1, color: null };
+			} else {
+				cell.setAttribute('style', `background-color: ${pixels[c + r * width]}`);
+			}
+			cell.addEventListener("click", function() {
 				fillSquare(cell, r, c);
 			});
-			cell.addEventListener("mouseover", mouseoverSquare);
-			cell.addEventListener("mouseleave", mouseleaveSquare);
+			cell.addEventListener("mouseover", function() {
+				mouseoverSquare(cell, r, c);
+			});
+			cell.addEventListener("mouseleave", function() {
+				mouseleaveSquare(cell, r, c);
+			});
 		}
     }
 }
@@ -61,22 +76,29 @@ function fillSquare(cell, r, c) {
 	var http = new XMLHttpRequest();
 	http.open('POST', url + 'api/pixels/' + c + '/' + r, true);
 	http.setRequestHeader('Content-type', 'application/json');
-	/*http.onload = function() {
-		console.log(`Recieved: ${this.responseText}`);
-		cell.setAttribute('style', `background-color: ${color.value}`);
-	}*/
+	ignoreCell = {c: c, r: r, color: color.value};
 
 	let send = JSON.stringify({color: color.value});
 	console.log(`Sending: ${send}`);
 	http.send(send);
+
+	if(hoverCell.c != -1 && hoverCell.r != -1) {
+		hoverCell.cell.id = 'normal-square';
+	}
+
+	couldInteract = false;
 }
 
-function mouseoverSquare() {
-	this.id = 'mouseover-square';
+function mouseoverSquare(cell, r, c) {
+	hoverCell = {c: c, r: r, cell: cell};
+
+	if(!canInteract()) return;
+	cell.id = 'mouseover-square';
 }
 
-function mouseleaveSquare() {
-	this.id = 'normal-square';
+function mouseleaveSquare(cell, r, c) {
+	hoverCell = {c: -1, r: -1, cell: null};
+	cell.id = 'normal-square';
 }
 
 function canInteract() {
@@ -87,7 +109,26 @@ function getTotalSeconds() {
 	return new Date().getTime() / 1000;
 }
 
+var couldInteract = false;
+
 function renderProgress() {
 	var progressBar = document.getElementById('progressBar')
 	progressBar.style.width = (Math.min((getTotalSeconds() - lastInteractTime) * 100 / interactInterval, 100)) + "%";
+
+	if(canInteract() && !couldInteract) {
+		couldInteract = true;
+		if(hoverCell.c != -1 && hoverCell.r != -1) {
+			hoverCell.cell.id = 'mouseover-square';
+		}
+	}
+}
+
+function getPlayerCount() {
+	var request = new XMLHttpRequest();
+	request.open('GET', url + 'api/playercount/');
+	request.addEventListener('load', function() {
+		var playerCount = JSON.parse(this.responseText);
+		document.getElementById('playerCount').innerHTML = `${playerCount} active now`;
+	});
+	request.send();
 }
